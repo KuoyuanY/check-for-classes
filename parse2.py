a3c0d19dac15d4a2b8acb5eefa990e593a96605c
Kuoyuan Yang <1kuoyuanyang@gmail.com>
2017-04-25 17:11:21 -0400

    this should not quit

--- parse2.py
+++ parse2.py
@@ -10,7 +10,7 @@ myNumber = ""
 initialTime = round(time.clock())
 keepRunning = True
 
-print("started running")
+
 def notify(message): #texts me with message
     client = Client(Account_SID, Auth_TOKEN)
     client.messages.create(
@@ -25,10 +25,12 @@ sections = {'0': '0'} #dictionary of sections
 response = urllib2.urlopen(url)
 html = response.read()
 html = html.decode("utf8")
+print("started running")
 
 #regex to find all sections
 while keepRunning:
-    currentTime = round(time.clock())
+    print("running in while loop")
+
     counter = 1
 
     for m in re.finditer('<span class="open-seats-count">', html): #finds all occurrences
@@ -36,13 +38,18 @@ while keepRunning:
         counter+=1
 
     for k, v in sections.items():
+        print time.asctime(time.localtime(time.time())) #prints current time
+        print(str(k) + "section:" + str(v)) #prints each section
         if not v == '0':
-            notify("a seated opened up!")
+            print("Done!")
+            notify("a seated opened up in section " + str(k))
             keepRunning = False #stops the bot from running
 
-    print("It's been a minute")
+
     time.sleep(60)#checks for open seats every 30 seconds
-    if currentTime-initialTime > 14400: #messages the user that bot is still running after every 6 hours
+    print("It's been a minute")
+    currentTime = round(time.clock())
+    if currentTime-initialTime >= 14400: #messages the user that bot is still running after every 4 hours
         initialTime = round(time.clock())
         notify("It's been 4 hours, bot is still checking for seats")
 notify("The script ended")
