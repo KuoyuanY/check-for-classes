0db1c3fe036b1caf093b8a13659e24dcb49a12fa
Kuoyuan Yang <1kuoyuanyang@gmail.com>
2017-04-26 14:41:03 -0400

    fixed section 0

--- parse.py
+++ parse.py
@@ -20,7 +20,7 @@ def notify(message): #texts me with message
     )
 
 url = "https://ntst.umd.edu/soc/search?courseId=HIST289y&sectionId=&termId=201708&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on"
-sections = {'0': '12'} #dictionary of sections
+sections = {'0': '2'} #dictionary of sections
 
 
 print("started running")
