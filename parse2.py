c4a79b6a85031b09a583103c97ff1af03c327005
Kuoyuan Yang <1kuoyuanyang@gmail.com>
2017-04-27 01:01:45 -0400

    time.clock kept returning zero, so used time.time instead

--- parse2.py
+++ parse2.py
@@ -7,7 +7,7 @@ Account_SID = ""
 Auth_TOKEN = ""
 myTwilioNumber = ""
 myNumber = ""
-initialTime = round(time.clock())
+initialTime = round(time.time())
 keepRunning = True
 
 
@@ -38,17 +38,18 @@ while keepRunning:
         counter+=1
 
     for k, v in sections.items():
-        print time.asctime(time.localtime(time.time())) #prints current time
-        print("section " + str(k) + ": " + str(v)) #prints each section
         if not v == '2':
             notify("a seated opened up in section " + str(k))
             keepRunning = False #stops the bot from running
 
 
     time.sleep(60)#checks for open seats every 30 seconds
-    print("It's been a minute")
-    currentTime = round(time.clock())
-    if currentTime-initialTime >= 14400: #messages the user that bot is still running after every 4 hours
-        initialTime = round(time.clock())
-        notify("It's been 4 hours, bot is still checking for seats")
+    print time.asctime(time.localtime(time.time())) #prints current time
+    currentTime = round(time.time())
+    print("It's been " + str((currentTime-initialTime)) + "seconds")
+    if currentTime-initialTime >= 72000: #messages the user that bot is still running after every 4 hours
+        initialTime = round(time.time())
+        print("been 20 hours")
+        notify("It's been 20 hours, bot is still checking for seats")
 notify("The script ended")
+print("over")
