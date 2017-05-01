92159c823b0eab3cacbd34edd1b1dd508575ab02
Kuoyuan Yang <1kuoyuanyang@gmail.com>
2017-05-01 09:04:42 -0400

    dont remember what i added

--- parse2.py
+++ parse2.py
@@ -30,28 +30,28 @@ while keepRunning:
 
     try:
         response = urllib2.urlopen(url)
+        html = response.read()
+        html = html.decode("utf8")
+
+        counter = 1
+
+        for m in re.finditer('<span class="total-seats-count">', html): #finds all occurrences
+            sections[counter] = html[m.end()+1] #put in dictionary
+            counter+=1
+
+        for k, v in sections.items():
+            if not v == '2':
+                notify("a seated opened up in section " + str(k))
+                keepRunning = False #stops the bot from running
     except urllib2.HTTPError:
+        print("http error")
         pass
-    html = response.read()
-    html = html.decode("utf8")
-
-    counter = 1
-
-    for m in re.finditer('<span class="total-seats-count">', html): #finds all occurrences
-        sections[counter] = html[m.end()+1] #put in dictionary
-        counter+=1
-
-    for k, v in sections.items():
-        if not v == '2':
-            notify("a seated opened up in section " + str(k))
-            keepRunning = False #stops the bot from running
-
 
     time.sleep(60)#checks for open seats every 30 seconds
     print time.asctime(time.localtime(time.time())) #prints current time
     currentTime = round(time.time())
     print("It's been " + str((currentTime-initialTime)) + "seconds")
-    if currentTime-initialTime >= 72000: #messages the user that bot is still running after every 4 hours
+    if currentTime-initialTime >= 72000: #messages the user that bot is still running after every 20 hours
         initialTime = round(time.time())
         print("been 20 hours")
         notify("It's been 20 hours, bot is still checking for seats")
